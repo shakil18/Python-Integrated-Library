7e8166aa6387243e3d7f74ab065d1e9f5f6a893c
Azizul Hakim Shakil <azizulhakim.shakil18@gmail.com>
2021-06-11 18:12:37 +0200

    web-service "hello world" program

--- client-app.py
+++ client-app.py
@@ -1,1 +1,9 @@
-print("====> Hello World! <====")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+app.run(host='0.0.0.0', port=81)
