7795771f6d1e0c189beb3f353945c53be7b430a7
Azizul Hakim Shakil <azizulhakim.shakil18@gmail.com>
2021-06-30 12:52:00 +0200

    Hello_world in Mono

---
+++ client-app.cs
@@ -0,0 +1,12 @@
+using System;
+ 
+namespace MonoExample
+{
+   public class HelloWorld
+   {
+      public static void Main(string[] args)
+      {
+         Console.WriteLine("==========> Hello World! <==========");
+      }
+   } 
+}
