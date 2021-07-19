ba433e442e62b57fb250d5f12f0ef8a4db0697b6
Azizul Hakim Shakil <azizulhakim.shakil18@gmail.com>
2021-07-19 17:53:14 +0200

    Update client-app.cs

--- client-app.cs
+++ client-app.cs
@@ -6,7 +6,7 @@ namespace MonoExample
    {
       public static void Main(string[] args)
       {
-         Console.WriteLine("==========> Hello World! <==========");
+         Console.WriteLine("============> Hello, World <============");
       }
    } 
 }
