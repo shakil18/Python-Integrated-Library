0870fbabddd9361b034bac66f8495e4ca2c62603
Azizul Hakim Shakil <azizulhakim.shakil18@gmail.com>
2021-07-28 01:12:50 +0200

    python dependency injection

--- client-app.py
+++ client-app.py
@@ -1,17 +1,13 @@
 import sys
-import os
 
 # To add library instead installing on the system
-LIBRARY_PATH = os.getcwd()
-LIBRARY_NAME = "requests"
-sys.path.append(os.path.join(LIBRARY_PATH, LIBRARY_NAME))
+LIBRARY_PATH = "/home/shakil/PycharmProjects/test_library"
+sys.path.append(LIBRARY_PATH)
 
 import requests as req
-
-print(f"============> Using 3rd party Library: '{LIBRARY_NAME}' <============")
 
 resp = req.get("http://www.webcode.me")
 print(f"============> HTTP Response messeage for 'http://www.webcode.me': {resp.status_code} <============")
 
 resp = req.get("http://www.webcode.me/news")
-print(f"============> HTTP Response messeage for 'http://www.webcode.me/news': {resp.status_code} <============")
+print(f"=
