2670ccbb4be41200d46202a252bc0a895b1edc0e
Azizul Hakim Shakil <azizulhakim.shakil18@gmail.com>
2021-07-27 16:15:05 +0200

    using library instead installing

--- client-app.py
+++ client-app.py
@@ -1,1 +1,15 @@
-print("============> Hello, World <============")
+import sys
+import os
+
+# To add library instead installing on the system
+LIBRARY_PATH = os.getcwd()
+LIBRARY_NAME = "requests"
+sys.path.append(os.path.join(LIBRARY_PATH, LIBRARY_NAME))
+
+import requests as req
+
+resp = req.get("http://www.webcode.me")
+print(f"============> {resp.status_code} <============")
+
+resp = req.get("http://www.webcode.me/news")
+print(f"============> {resp.status_code} <============")
