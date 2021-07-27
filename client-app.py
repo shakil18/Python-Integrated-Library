cc8b7a0820a5a87bb350385d89c2b2f9bbff4863
Azizul Hakim Shakil <azizulhakim.shakil18@gmail.com>
2021-07-27 16:18:08 +0200

    Update client-app.py

--- client-app.py
+++ client-app.py
@@ -8,8 +8,10 @@ sys.path.append(os.path.join(LIBRARY_PATH, LIBRARY_NAME))
 
 import requests as req
 
+print(f"============> Using 3rd party Library: '{LIBRARY_NAME}' <============")
+
 resp = req.get("http://www.webcode.me")
-print(f"============> {resp.status_code} <============")
+print(f"============> HTTP Response messeage for 'http://www.webcode.me': {resp.status_code} <============")
 
 resp = req.get("http://www.webcode.me/news")
-print(f"============> {resp.status_code} <============")
+print(f"============> HTTP Response messeage for 'http://www.webcode.me/news': {resp.status_code} <============")
